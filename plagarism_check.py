e2b6f66361125c0140d6d85739b678bdfe730604
Arthur <arthur.bowers@phabrix.com>
2022-04-25 14:44:15 +0100

    Script to check for plagarism between 2 files.

---
+++ plagarism_check.py
@@ -0,0 +1,13 @@
+from difflib import SequenceMatcher
+
+
+def plagerism_checker(f1, f2):
+    with open(f1, errors="ignore") as file1, open(f2, errors="ignore") as file2:
+        f1_data = file1.read()
+        f2_data = file2.read()
+        checking = SequenceMatcher(None, f1_data, f2_data).ratio()
+
+    print(f"These files are {checking*100} % similar.")
+
+    file1 = input("Enter file 1 path: ")
+    file2 = input("Enter file 2 path: ")
